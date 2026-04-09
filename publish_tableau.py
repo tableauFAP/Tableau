f4f4c753da9a034652e219178b436c8946284cc4
tableauFAP <fap.tableau@citiz.fr>
2026-04-09 12:21:39 +0200

    Update publish_tableau.py

--- publish_tableau.py
+++ publish_tableau.py
@@ -3,27 +3,31 @@ import os
 from tableau_api_lib import TableauServerConnection
 from tableau_api_lib.utils.querying import get_projects_dataframe
 
-# ⚠️ Ces variables seront passées depuis GitHub Actions secrets
-TABLEAU_SERVER_URL = os.environ['TABLEAU_SERVER_URL']  # ex: https://10ax.online.tableau.com
-TABLEAU_SITE_ID = os.environ['TABLEAU_SITE_ID']        # ex: Default
+# Variables d'environnement (passées depuis GitHub Actions secrets)
+TABLEAU_SERVER_URL = os.environ['TABLEAU_SERVER_URL']      # ex: https://10ax.online.tableau.com
+TABLEAU_SITE_ID = os.environ['TABLEAU_SITE_ID']            # ex: Default
 TABLEAU_API_TOKEN_NAME = os.environ['TABLEAU_API_TOKEN_NAME']
 TABLEAU_API_TOKEN_VALUE = os.environ['TABLEAU_API_TOKEN_VALUE']
 
-# Fichier CSV à publier
 CSV_FILE_PATH = "data_carburant.csv"
 DATASOURCE_NAME = "Carburant"
-PROJECT_NAME = "Default"  # Nom du projet Tableau Cloud où publier
+PROJECT_NAME = "Default"  # Nom du projet Tableau Cloud
 
 # --- Connexion à Tableau Cloud ---
-tableau_config = {
-    "server": TABLEAU_SERVER_URL,
-    "api_version": "3.20",
-    "personal_access_token_name": TABLEAU_API_TOKEN_NAME,
-    "personal_access_token_secret": TABLEAU_API_TOKEN_VALUE,
-    "site_id": TABLEAU_SITE_ID
-}
+try:
+    connection = TableauServerConnection(
+        server=TABLEAU_SERVER_URL,
+        site_id=TABLEAU_SITE_ID,
+        personal_access_token_name=TABLEAU_API_TOKEN_NAME,
+        personal_access_token_secret=TABLEAU_API_TOKEN_VALUE
+    )
+except TypeError:
+    # Si l'ancienne version ne supporte pas ces arguments, afficher un message clair
+    raise Exception(
+        "Erreur de version de tableau-api-lib : "
+        "Assurez-vous d'avoir installé la dernière version via pip install --upgrade tableau-api-lib"
+    )
 
-connection = TableauServerConnection.from_config_dict(config_json=tableau_config)
 connection.sign_in()
 print("✅ Connecté à Tableau Cloud")
 
